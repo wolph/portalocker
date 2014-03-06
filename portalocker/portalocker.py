79418fad8e95d1475aaaddff2ecf868aef75dacc
anatoly techtonik <techtonik@gmail.com>
2014-03-06 10:25:46 +0200

    Django version of portalocker

--- portalocker/portalocker.py
+++ portalocker/portalocker.py
@@ -1,138 +1,113 @@
-# portalocker.py - Cross-platform (posix/nt) API for flock-style file locking.
-#                  Requires python 1.5.2 or better.
-'''Cross-platform (posix/nt) API for flock-style file locking.
+"""
+Portable file locking utilities.
 
-Synopsis:
+Based partially on an example by Jonathan Feignberg in the Python
+Cookbook [1] (licensed under the Python Software License) and a ctypes port by
+Anatoly Techtonik for Roundup [2] (license [3]).
 
-   import portalocker
-   file = open('somefile', 'r+')
-   portalocker.lock(file, portalocker.LOCK_EX)
-   file.seek(12)
-   file.write('foo')
-   file.close()
+[1] http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/65203
+[2] http://sourceforge.net/p/roundup/code/ci/default/tree/roundup/backends/portalocker.py
+[3] http://sourceforge.net/p/roundup/code/ci/default/tree/COPYING.txt
 
-If you know what you're doing, you may choose to
+Example Usage::
 
-   portalocker.unlock(file)
-
-before closing the file, but why?
-
-Methods:
-
-   lock( file, flags )
-   unlock( file )
-
-Constants:
-
-   LOCK_EX
-   LOCK_SH
-   LOCK_NB
-
-Exceptions:
-
-    LockException
-
-Notes:
-
-For the 'nt' platform, this module requires the Python Extensions for Windows.
-Be aware that this may not work as expected on Windows 95/98/ME.
-
-History:
-
-I learned the win32 technique for locking files from sample code
-provided by John Nielsen <nielsenjf@my-deja.com> in the documentation
-that accompanies the win32 modules.
-
-Author: Jonathan Feinberg <jdf@pobox.com>,
-        Lowell Alleman <lalleman@mfps.com>
-Version: $Id: portalocker.py 5474 2008-05-16 20:53:50Z lowell $
-
-'''
-
-
-__all__ = [
-    'lock',
-    'unlock',
-    'LOCK_EX',
-    'LOCK_SH',
-    'LOCK_NB',
-    'LockException',
-]
-
+    >>> from django.core.files import locks
+    >>> with open('./file', 'wb') as f:
+    ...     locks.lock(f, locks.LOCK_EX)
+    ...     f.write('Django')
+"""
 import os
 
+__all__ = ('LOCK_EX', 'LOCK_SH', 'LOCK_NB', 'lock', 'unlock')
 
-class LockException(Exception):
-    # Error codes:
-    LOCK_FAILED = 1
 
-if os.name == 'nt':  # pragma: no cover
-    import win32con
-    import win32file
-    import pywintypes
-    LOCK_EX = win32con.LOCKFILE_EXCLUSIVE_LOCK
+def _fd(f):
+    """Get a filedescriptor from something which could be a file or an fd."""
+    return f.fileno() if hasattr(f, 'fileno') else f
+
+
+if os.name == 'nt':
+    import msvcrt
+    from ctypes import (sizeof, c_ulong, c_void_p, c_int64,
+                        Structure, Union, POINTER, windll, byref)
+    from ctypes.wintypes import BOOL, DWORD, HANDLE
+
     LOCK_SH = 0  # the default
-    LOCK_NB = win32con.LOCKFILE_FAIL_IMMEDIATELY
-    # is there any reason not to reuse the following structure?
-    __overlapped = pywintypes.OVERLAPPED()
+    LOCK_NB = 0x1  # LOCKFILE_FAIL_IMMEDIATELY
+    LOCK_EX = 0x2  # LOCKFILE_EXCLUSIVE_LOCK
+
+    # --- Adapted from the pyserial project ---
+    # detect size of ULONG_PTR
+    if sizeof(c_ulong) != sizeof(c_void_p):
+        ULONG_PTR = c_int64
+    else:
+        ULONG_PTR = c_ulong
+    PVOID = c_void_p
+
+    # --- Union inside Structure by stackoverflow:3480240 ---
+    class _OFFSET(Structure):
+        _fields_ = [
+            ('Offset', DWORD),
+            ('OffsetHigh', DWORD)]
+
+    class _OFFSET_UNION(Union):
+        _anonymous_ = ['_offset']
+        _fields_ = [
+            ('_offset', _OFFSET),
+            ('Pointer', PVOID)]
+
+    class OVERLAPPED(Structure):
+        _anonymous_ = ['_offset_union']
+        _fields_ = [
+            ('Internal', ULONG_PTR),
+            ('InternalHigh', ULONG_PTR),
+            ('_offset_union', _OFFSET_UNION),
+            ('hEvent', HANDLE)]
+
+    LPOVERLAPPED = POINTER(OVERLAPPED)
+
+    # --- Define function prototypes for extra safety ---
+    LockFileEx = windll.kernel32.LockFileEx
+    LockFileEx.restype = BOOL
+    LockFileEx.argtypes = [HANDLE, DWORD, DWORD, DWORD, DWORD, LPOVERLAPPED]
+    UnlockFileEx = windll.kernel32.UnlockFileEx
+    UnlockFileEx.restype = BOOL
+    UnlockFileEx.argtypes = [HANDLE, DWORD, DWORD, DWORD, LPOVERLAPPED]
+
+    def lock(f, flags):
+        hfile = msvcrt.get_osfhandle(_fd(f))
+        overlapped = OVERLAPPED()
+        ret = LockFileEx(hfile, flags, 0, 0, 0xFFFF0000, byref(overlapped))
+        return bool(ret)
+
+    def unlock(f):
+        hfile = msvcrt.get_osfhandle(_fd(f))
+        overlapped = OVERLAPPED()
+        ret = UnlockFileEx(hfile, 0, 0, 0xFFFF0000, byref(overlapped))
+        return bool(ret)
+
 elif os.name == 'posix':
     import fcntl
+    LOCK_SH = fcntl.LOCK_SH  # shared lock
+    LOCK_NB = fcntl.LOCK_NB  # non-blocking
     LOCK_EX = fcntl.LOCK_EX
-    LOCK_SH = fcntl.LOCK_SH
-    LOCK_NB = fcntl.LOCK_NB
-else:  # pragma: no cover
-    raise RuntimeError('PortaLocker only defined for nt and posix platforms')
 
+    def lock(f, flags):
+        ret = fcntl.flock(_fd(f), flags)
+        return (ret == 0)
 
-def nt_lock(file_, flags):  # pragma: no cover
-    hfile = win32file._get_osfhandle(file_.fileno())
-    try:
-        win32file.LockFileEx(hfile, flags, 0, -0x10000, __overlapped)
-    except pywintypes.error, exc_value:
-        # error: (33, 'LockFileEx', 'The process cannot access the file
-        # because another process has locked a portion of the file.')
-        if exc_value[0] == 33:
-            raise LockException(LockException.LOCK_FAILED, exc_value[2])
-        else:
-            # Q:  Are there exceptions/codes we should be dealing with
-            # here?
-            raise
+    def unlock(f):
+        ret = fcntl.flock(_fd(f), fcntl.LOCK_UN)
+        return (ret == 0)
 
+else:  # File locking is not supported.
+    LOCK_EX = LOCK_SH = LOCK_NB = 0
 
-def nt_unlock(file_):  # pragma: no cover
-    hfile = win32file._get_osfhandle(file_.fileno())
-    try:
-        win32file.UnlockFileEx(hfile, 0, -0x10000, __overlapped)
-    except pywintypes.error, exc_value:
-        if exc_value[0] == 158:
-            # error: (158, 'UnlockFileEx', 'The segment is already '
-            #         'unlocked.')
-            # To match the 'posix' implementation, silently ignore this
-            # error
-            pass
-        else:
-            # Q:  Are there exceptions/codes we should be dealing with
-            # here?
-            raise
+    # Dummy functions that don't do anything.
+    def lock(f, flags):
+        # File is not locked
+        return False
 
-
-def posix_lock(file_, flags):
-    try:
-        fcntl.flock(file_.fileno(), flags)
-    except IOError, exc_value:
-        # The exception code varies on different systems so we'll catch
-        # every IO error
-        raise LockException(*exc_value)
-
-
-def posix_unlock(file_):
-    fcntl.flock(file_.fileno(), fcntl.LOCK_UN)
-
-if os.name == 'nt':  # pragma: no cover
-    lock = nt_lock
-    unlock = nt_unlock
-elif os.name == 'posix':
-    lock = posix_lock
-    unlock = posix_unlock
-else:  # pragma: no cover
-    raise RuntimeError('Your os %r is unsupported.' % os.name)
+    def unlock(f):
+        # File is unlocked
+        return True
