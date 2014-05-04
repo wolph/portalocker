141d45dc7d56f7b04038ac128bb7865da902043a
Rick van Hattem <Wolph@wol.ph>
2014-05-04 14:35:11 +0200

    bumped version for adding tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 author = 'Rick van Hattem'
 email = 'Rick.van.Hattem@Fawo.nl'
-version = '0.5.1'
+version = '0.5.2'
 desc = '''Wraps the portalocker recipe for easy usage'''
 
 extra = {}
