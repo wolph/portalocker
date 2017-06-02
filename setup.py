878801586aea075f70c9ebca31da825ad4505915
Rick van Hattem <Wolph@wol.ph>
2017-06-02 17:06:22 +0200

    removed old python versions which are not supported by 3rd party libraries anymore

--- setup.py
+++ setup.py
@@ -85,11 +85,11 @@ if __name__ == '__main__':
         classifiers=[
             'Intended Audience :: Developers',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy',
         ],
