98f6bfc1a80bc93a0169beb5cbd14287a81b8d4b
Rick van Hattem <Wolph@wol.ph>
2020-04-12 17:56:54 +0200

    Incrementing version to v1.7.0

--- portalocker/__init__.py
+++ portalocker/__init__.py
@@ -11,7 +11,7 @@ __author__ = __about__.__author__
 #: Current author's email address
 __email__ = __about__.__email__
 #: Version number
-__version__ = '1.6.0'
+__version__ = '1.7.0'
 #: Package description for Pypi
 __description__ = __about__.__description__
 #: Package homepage
