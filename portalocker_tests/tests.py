fccafdd66eda3d7752067785bb28f9387fe74d48
Rick van Hattem <Wolph@wol.ph>
2022-07-09 17:08:29 +0200

    skip linux/unix/mac tests on windows

--- portalocker_tests/tests.py
+++ portalocker_tests/tests.py
@@ -228,7 +228,8 @@ def test_blocking_timeout(tmpfile):
         lock.acquire(timeout=5)
 
 
-@pytest.mark.skipif(os.name == 'nt')
+@pytest.mark.skipif(os.name == 'nt',
+                    reason='Windows uses an entirely different lockmechanism')
 def test_nonblocking(tmpfile):
     with open(tmpfile, 'w') as fh:
         with pytest.raises(RuntimeError):
