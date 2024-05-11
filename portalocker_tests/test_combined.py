5ee48d82c6211b010eaf8920ecece853c2e67630
Rick van Hattem <Wolph@wol.ph>
2024-05-11 15:27:16 +0200

    making ruff happy for tests

--- portalocker_tests/test_combined.py
+++ portalocker_tests/test_combined.py
@@ -7,10 +7,10 @@ def test_combined(tmpdir):
     output_file = tmpdir.join('combined.py')
     __main__.main(['combine', '--output-file', output_file.strpath])
 
-    print(output_file)
-    print('#################')
-    print(output_file.read())
-    print('#################')
+    print(output_file)  # noqa: T201
+    print('#################')  # noqa: T201
+    print(output_file.read())  # noqa: T201
+    print('#################')  # noqa: T201
 
     sys.path.append(output_file.dirname)
     # Combined is being generated above but linters won't understand that
