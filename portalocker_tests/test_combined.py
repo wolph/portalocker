becd9d10716df86aff34816ff62273ec3ec2143d
Rick van Hattem <Wolph@wol.ph>
2024-05-11 15:25:51 +0200

    debugging combined file output

--- portalocker_tests/test_combined.py
+++ portalocker_tests/test_combined.py
@@ -6,6 +6,12 @@ from portalocker import __main__
 def test_combined(tmpdir):
     output_file = tmpdir.join('combined.py')
     __main__.main(['combine', '--output-file', output_file.strpath])
+
+    print(output_file)
+    print('#################')
+    print(output_file.read())
+    print('#################')
+
     sys.path.append(output_file.dirname)
     # Combined is being generated above but linters won't understand that
     import combined  # type: ignore
